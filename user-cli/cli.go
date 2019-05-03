869f779895fc25785c9fe71f085f05ae558d41ca
Joe Glista <jglista@dealertire.com>
2019-05-03 12:31:23 -0400

    Fix Issues With user-service, user-cli
    * Fix issue where the user-service could not find the postgres host, the connection string was malformed, the fix follows the v11 format.
    * Fix issue where user-service stopped after attempting one connection to postgres. A health was added at startup to check when postgres has started. The user-service has a restart on-failure option that allows it to 
    retry the connection until postgres is ready.
    * All Dockerfiles are now tagged at Go 1.12.x to allow us to pull in Go 1.12 minor version patches.
    * user-cli has been refactored slightly.

--- user-cli/cli.go
+++ user-cli/cli.go
@@ -4,25 +4,22 @@ import (
 	"log"
 	"os"
 
-	pb "github.com/jglista/go-microservices/user-service/proto/user"
 	"github.com/micro/cli"
+
+	pb "github.com/jglista/go-microservices/user-service/proto/user"
 	"github.com/micro/go-micro"
 	microclient "github.com/micro/go-micro/client"
-	"github.com/micro/go-micro/cmd"
 	"golang.org/x/net/context"
 )
 
 func main() {
-	cmd.Init()
-
-	// Create new greet client
-	client := pb.NewUserService("go.micro.srv.user", microclient.DefaultClient)
-
-	service := micro.NewService(
+	srv := micro.NewService(
+		micro.Name("go.micro.srv.user-cli"),
+		micro.Version("latest"),
 		micro.Flags(
 			cli.StringFlag{
 				Name:  "name",
-				Usage: "Your full name",
+				Usage: "Your name",
 			},
 			cli.StringFlag{
 				Name:  "email",
@@ -39,38 +36,36 @@ func main() {
 		),
 	)
 
-	service.Init(
-		micro.Action(func(c *cli.Context) {
-			name := c.String("name")
-			email := c.String("email")
-			password := c.String("password")
-			company := c.String("company")
+	srv.Init(micro.Action(func(c *cli.Context) {
+		// Check to ensure requried arguments were supplied to the cli
+		if (c.String("name") == "") ||
+			(c.String("email") == "") ||
+			(c.String("password") == "") ||
+			(c.String("company") == "") {
+			log.Fatal("name, email, password, and company are required and were not passed in")
+		}
 
-			r, err := client.Create(context.TODO(), &pb.User{
-				Name:     name,
-				Email:    email,
-				Password: password,
-				Company:  company,
-			})
-			if err != nil {
-				log.Fatalf("Could not create: %v", err)
-			}
-			log.Printf("Created: %t", r.User.Id)
+		client := pb.NewUserService("go.micro.srv.user", microclient.DefaultClient)
 
-			getAll, err := client.GetAll(context.Background(), &pb.Request{})
-			if err != nil {
-				log.Fatalf("Could not list users: %v", err)
-			}
-			for _, v := range getAll.Users {
-				log.Println(v)
-			}
+		r, err := client.Create(context.TODO(), &pb.User{
+			Name:     c.String("name"),
+			Email:    c.String("email"),
+			Password: c.String("password"),
+			Company:  c.String("company"),
+		})
+		if err != nil {
+			log.Fatalf("Could not create: %v", err)
+		}
+		log.Printf("Created: %s", r.User.Id)
 
-			// Let's just exit because
-			os.Exit(0)
-		}),
-	)
-	// Run the server
-	if err := service.Run(); err != nil {
-		log.Println(err)
-	}
+		getAll, err := client.GetAll(context.Background(), &pb.Request{})
+		if err != nil {
+			log.Fatalf("Could not list users: %v", err)
+		}
+		for _, v := range getAll.Users {
+			log.Println(v)
+		}
+
+		os.Exit(0)
+	}))
 }
